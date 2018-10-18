8be784617dfc1df60c4869106a6e894a0adad498
MahmoudSaleh15 <43014605+MahmoudSaleh15@users.noreply.github.com>
2018-10-18 13:24:14 -0600

    ddd

--- scripts/LandscapePortait.js
+++ scripts/LandscapePortait.js
@@ -1,1 +1,46 @@
-console.log("You made it to the LandscapePortait.js!");
+//console.log("You made it to the LandscapePortait.js!");
+
+
+let stop = false;
+
+function testNaN (number) {
+  console.log(stop);
+  if ( isNaN (number) ) {
+    stop = true;
+    return "Type a Real Number"
+  } else {
+    return "Input Validated"
+  }
+
+}
+
+function geometry (width, height) { // Notice these parape
+  if (width == height) return 'Square';
+
+}
+
+// This coding illustrates the advanced method
+function main () {
+
+  let firstNumber, secondNumber, text;
+
+  firstNumber = document.getElementById("textfield1").value;
+
+  document.getElementById("validityTest1").innerHTML = alert( testNaN (firstNumber) );
+  document.getElementById("validityTest1").innerHTML = testNaN (firstNumber);
+
+  secondNumber = document.getElementById("testfield2").value;
+
+  document.getElementById("validityTest2").innerHTML = alert( testNaN (secondNumber) );
+  document.getElementById("validityTest2").innerHTML = testNaN (secondNumber);
+
+  if (stop == true)
+  {
+    document.getElementById("large").innerHTML = "Restart because I said so."
+  }
+    else
+    {
+      console.log("What did you say", geometry (firstNumber, secondNumber));
+      document.getElementById("large").innerHTML = "What did you say " + geometry (firstNumber, secondNumber);
+    }
+  }
--- scripts/childTemplate.js
+++ scripts/childTemplate.js
@@ -1,1 +1,26 @@
-console.log("You made it to the childTemplate.js!");
+console.log("You made it to the childTemplate.js!"); //Debugging line to console ensuring to console ensuring JavaScript link
+
+function main ()
+{
+  let firstNumber, secondNumber, text;
+
+  firstNumber = document.getElementById("textfield1").value;
+
+  document.getElementById("validityTest1").innerHTML = alert( testNaN (firstNumber) );
+  document.getElementById("validityTest1").innerHTML = testNaN (firstNumber);
+
+  secondNumber = document.getElementById("testfield2").value;
+
+  document.getElementById("validityTest2").innerHTML = alert( testNaN (secondNumber) );
+  document.getElementById("validityTest2").innerHTML = testNaN (secondNumber);
+
+  if (stop == true)
+  {
+    document.getElementById("large").innerHTML = "Restart because I said so."
+  }
+    else
+    {
+      console.log("What did you say", geometry (firstNumber, secondNumber));
+      document.getElementById("large").innerHTML = "What did you say " + geometry (firstNumber, secondNumber);
+    }
+  }
